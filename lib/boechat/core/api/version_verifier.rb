bc331a3faf64e6c2736b536f5e488c6ceb31141f
Danilo Barion Nogueira <danilo.barion1986@live.com>
2018-04-28 16:33:47 -0300

    Adjust requires

--- lib/boechat/core/api/version_verifier.rb
+++ lib/boechat/core/api/version_verifier.rb
@@ -2,11 +2,47 @@
 
 require_relative './request'
 require_relative './requests'
+require_relative '../config_reader'
+require 'typhoeus'
+require 'json'
 
 module Boechat
   module Core
     module Api
+      # Class responsible for call the endpoints and return all the results
       class VersionVerifier
+        attr_reader :config
+
+        def initialize
+          @config = ConfigReader.new.call
+        end
+
+        def call
+          results = call_endpoints
+
+          results.map do |response|
+            JSON.parse(response.body)
+          end
+        end
+
+        private
+
+        def call_endpoints
+          requests = create_requests
+          [].tap do |result|
+            requests.each_pair do |_k, v|
+              result << Typhoeus.get(v)
+            end
+          end
+        end
+
+        def create_requests
+          requests = Requests.new
+          @config.each_with_index do |config, index|
+            requests[index.to_s] = config
+          end
+          requests.requests
+        end
       end
     end
   end
--- lib/boechat/core/config_reader.rb
+++ lib/boechat/core/config_reader.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
 
+require 'yaml'
+
 module Boechat
   module Core
+    # Class responsible for read the boechat.yml file, that describes all endpoints to call
     class ConfigReader
+      attr_reader :config_path
+
+      def initialize
+        app_root = Pathname.new(File.expand_path('../../../', __dir__))
+        @config_path = File.join(app_root, 'config', 'boechat.yml')
+      end
+
+      def call
+        YAML.safe_load(File.read(@config_path))
+      end
     end
   end
 end
