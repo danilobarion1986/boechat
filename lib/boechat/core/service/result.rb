e527c0a670cdf097aec49e1829605df4eed2077f
Danilo Barion Nogueira <danilo.barion1986@live.com>
2018-04-30 11:07:10 -0300

    Includes Result class

---
+++ lib/boechat/core/service/result.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'oj'
+
+module Boechat
+  module Core
+    module Service
+      # Class responsible for store the response of the Request class
+      class Result
+        attr_reader :parsed_response, :success, :failure
+        alias success? success
+        alias failure? failure
+
+        def initialize(parsed_response)
+          @parsed_response = parsed_response
+        end
+
+        def success?
+          @parsed_response && @parsed_response[:status] < 400
+        end
+
+        def failure?
+          @parsed_response && @parsed_response[:status] >= 400
+        end
+      end
+    end
+  end
+end
