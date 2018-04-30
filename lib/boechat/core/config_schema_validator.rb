06a7c542a18e57f6322f824c1fb1063b88ecd932
Danilo Barion Nogueira <danilo.barion1986@live.com>
2018-04-30 12:33:47 -0300

    Includes class to validate config schema

---
+++ lib/boechat/core/config_schema_validator.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'dry-validation'
+
+module Boechat
+  module Core
+    # Class responsible for validate the schema of the boechat.yml configuration file
+    class ConfigSchemaValidator
+      CONFIG_SCHEMA = Dry::Validation.Schema do
+        required('services').each do
+          schema do
+            required('name').filled(:str?)
+            required('base_url').filled(:str?)
+            optional('status_endpoint').filled(:str?)
+            optional('http_verb').filled(included_in?: %w[get post])
+          end
+        end
+      end
+
+      def self.call(config)
+        CONFIG_SCHEMA.call(YAML.safe_load(config))
+      end
+    end
+  end
+end
