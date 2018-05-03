fdd63f6974c82173cf7595ec6c494c7e134e4a23
Danilo Barion Nogueira <danilo.barion1986@live.com>
2018-05-03 20:44:27 -0300

    Add Main class to development/debug

--- lib/boechat/core.rb
+++ lib/boechat/core.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
 require 'boechat/internal/version'
+require 'boechat/core/service/verifier'
+require 'pry'
 
 # :nodoc:
 module Boechat
   # :nodoc:
   module Core
+    class Main
+      def initialize
+        verifier = Boechat::Core::Service::Verifier.new
+        puts verifier.call.call.output
+      end
+    end
   end
 end
