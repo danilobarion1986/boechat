9329d69413856975624218ed88ba6463c4566bc8
Danilo Barion Nogueira <danilo.barion1986@live.com>
2018-05-03 14:46:05 -0300

    Fix tests subject order

--- spec/boechat/core/service/request_list_spec.rb
+++ spec/boechat/core/service/request_list_spec.rb
@@ -1,23 +1,24 @@
 # frozen_string_literal: true
 
 require 'boechat/core/service/request_list'
-require 'pry'
+require 'boechat/core/service/requester'
 
 RSpec.describe Boechat::Core::Service::RequestList do
-  let(:requester_one) { -> { puts "I'm the one!" } }
-  let(:requester_two) { -> { puts "I'm the two!" } }
+  let(:url) { 'http://api.example.com' }
+  let(:requester_one) { Boechat::Core::Service::Requester.new(url) }
+  let(:requester_two) { Boechat::Core::Service::Requester.new(url) }
 
   before { allow_any_instance_of(Proc).to receive(:call) }
-  subject { described_class.new(requester_one: requester_one, requester_two: requester_two) }
+  subject { described_class.new({ requester_one: requester_one, requester_two: requester_two}) }
 
   describe '#call' do
     context 'when an requester identifier IS passed' do
       context 'when the identifier EXISTS in requesters hash' do
         it 'run the correct requester' do
-          subject.call(:requester_one)
+          expect(requester_one).to receive(:call).once
+          expect(requester_two).not_to receive(:call)
 
-          expect(requester_one).to have_received(:call).once
-          expect(requester_two).not_to have_received(:call)
+          subject.call(:requester_one)
         end
 
         it 'return the correct object' do
@@ -29,10 +30,10 @@ RSpec.describe Boechat::Core::Service::RequestList do
 
       context 'when the identifier NOT EXISTS in requesters hash' do
         it "don't run any requester" do
-          subject.call(:requester_three)
+          expect(requester_one).not_to receive(:call)
+          expect(requester_two).not_to receive(:call)
 
-          expect(requester_one).not_to have_received(:call)
-          expect(requester_two).not_to have_received(:call)
+          subject.call(:requester_three)
         end
 
         it 'return the correct object' do
@@ -45,7 +46,11 @@ RSpec.describe Boechat::Core::Service::RequestList do
 
     context 'when an requester identifier IS NOT passed' do
       context 'when the identifier NOT EXISTS in requesters hash' do
-        xit 'run all the requesters' do
+        it 'run all the requesters' do
+          expect(requester_one).to receive(:call).once
+          expect(requester_two).to receive(:call).once
+
+          subject.call
         end
       end
     end
