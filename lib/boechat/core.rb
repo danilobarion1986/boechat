bccc3f02a8872007f9b7fb129f50e60311818ae7
Danilo Barion Nogueira <danilo.barion1986@live.com>
2018-05-04 14:51:38 -0300

    Remove unused code

--- lib/boechat/core.rb
+++ lib/boechat/core.rb
@@ -8,11 +8,5 @@ require 'pry'
 module Boechat
   # :nodoc:
   module Core
-    class Main
-      def initialize
-        verifier = Boechat::Core::Service::Verifier.new
-        puts verifier.call.call.output
-      end
-    end
   end
 end
