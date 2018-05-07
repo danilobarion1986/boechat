afa3af5b2f4e1f8a7a5928056742d2d00dbcf360
Danilo Barion Nogueira <danilo.barion1986@live.com>
2018-05-07 00:21:36 -0300

    Simplifies code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,71 +1,26 @@
 # frozen_string_literal: true
 
-if RUBY_ENGINE == 'ruby' && ENV['COVERAGE'] == 'true'
-  require 'yaml'
-  rubies = YAML.safe_load(File.read(File.join(__dir__, '..', '.travis.yml')))['rvm']
-  latest_mri = rubies.select { |v| v =~ /\A\d+\.\d+.\d+\z/ }.max
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec/'
 
-  if RUBY_VERSION == latest_mri
-    require 'simplecov'
-    SimpleCov.start do
-      add_filter '/spec/'
-    end
-  end
+  add_group 'Core', 'lib/boechat'
+  add_group 'Config', 'lib/boechat/core/config'
+  add_group 'Service', 'lib/boechat/core/service'
 end
 
-$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
-require 'rspec/version'
+RSpec::Mocks.configuration.allow_message_expectations_on_nil = true
 
-begin
-  require 'pry'
-  require 'pry-byebug'
-rescue LoadError
-end
-
-require 'boechat/core'
-
-module Test
-  def self.remove_constants
-    constants.each(&method(:remove_const))
-  end
-end
-
-# See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  raise 'This condition block can be safely removed' unless RSpec::Version::STRING >= '4.0.0'
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
-
+  config.disable_monkey_patching!
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
 
-  config.shared_context_metadata_behavior = :apply_to_host_groups
-
-  config.after do
-    Test.remove_constants
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
   end
 
-  # This allows you to limit a spec run to individual examples or groups
-  # you care about by tagging them with `:focus` metadata. When nothing
-  # is tagged with `:focus`, all examples get run. RSpec also provides
-  # aliases for `it`, `describe`, and `context` that include `:focus`
-  # metadata: `fit`, `fdescribe` and `fcontext`, respectively.
-  config.filter_run_when_matching :focus
-
-  # Allows RSpec to persist some state between runs in order to support
-  # the `--only-failures` and `--next-failure` CLI options.
-  config.example_status_persistence_file_path = 'spec/examples.txt'
-
-  config.disable_monkey_patching!
-
-  config.warnings = true
-
-  # Use the documentation formatter for detailed output
-  config.default_formatter = 'doc' if config.files_to_run.one?
-
   config.order = :random
-
   Kernel.srand config.seed
 end
